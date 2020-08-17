631b680039d01a92cda54bb9157f5ae6c44764d6
ibnuminati <ibnuminati@gmail.com>
2020-08-17 11:06:47 +0700

    Latihan Membuat app-bar Component

--- ClubFinder/app.js
+++ ClubFinder/app.js
@@ -1,3 +1,4 @@
+import "./src/script/component/app-bar.js"; // agar kode pada berkas app-bar.js tereksekusi
 import main from "./src/script/view/main.js";
 
 document.addEventListener("DOMContentLoaded", main);
