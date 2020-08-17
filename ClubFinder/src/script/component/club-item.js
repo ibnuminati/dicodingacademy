8cafaff316eaad09cc5e70e6ad79b83e9d0acbdf
ibnuminati <ibnuminati@gmail.com>
2020-08-17 12:28:36 +0700

    Latihan Membuat club-list dan club-item Component

---
+++ ClubFinder/src/script/component/club-item.js
@@ -0,0 +1,18 @@
+class ClubItem extends HTMLElement {
+    // Fungsi setter club berfungsi untuk menetapkan nilai club ke properti this._club yang nantinya akan digunakan pada fungsi render untuk menampilkan data individual club hasil pencarian.
+    set club(club) {
+        this._club = club;
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+            <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+            <div class="club-info">
+                <h2>${this._club.name}</h2>
+                <p>${this._club.description}</p>
+            </div>`;
+    } // copy dari fungsi renderResult di berkas main.js
+}
+
+customElements.define("club-item", ClubItem); // mendefinisikan custom element yang kita buat agar dapat digunakan pada DOM
